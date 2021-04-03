06a4517250d02f196ac88e406d28e26177ee1aad
Rodolfo J. Meneses Leal <rodojml@me.com>
2021-04-03 17:07:26 -0600

    Starting with the first part of the project for Programacion I. This is the first update, setting the bases for the program.

---
+++ src/Project1/Launcher.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project1;
+
+/**
+ *
+ * @author rodo
+ */
+public class Launcher
+{
+    public void main(String[] args)
+    {
+        while(true)
+        {
+            //Make a menu here to ask user which part of the project wants to run
+        }
+    }
+    
+    public void problem1()
+    {
+        
+        
+    }
+    
+    public void problem2()
+    {
+        
+        
+    }
+}
---
+++ src/Project1/Problem1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project1;
+import java.util.Scanner; 
+
+/**
+ * @author Rodolfo Meneses Leal 20200112770
+ */
+public class Problem1 
+{
+    private int dollar;     // 500
+    private int euro;       // 765
+    private int colon;      // 1
+    private int userUnits;
+    private String foreignExchange;
+    
+    Scanner read = new Scanner(System.in);
+    
+    
+    public void setUserUnits(int userUnits) 
+    {
+        this.userUnits = userUnits;
+    }
+    
+    
+    public void exchange()
+    {
+        
+    }
+    
+}
