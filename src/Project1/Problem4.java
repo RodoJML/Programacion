3c2dc907783b93ad7e3391e64cd53f16a12be762
Rodolfo J. Meneses Leal <rodojml@me.com>
2021-04-07 00:27:30 -0600

    Update of project, solving problem 4 of 5 Skipped 3 since I need to research if other kind of class or project is required.

---
+++ src/Project1/Problem4.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project1;
+
+/**
+ *
+ * @author rodo
+ */
+public class Problem4 
+{
+    
+    int[] numbersArray = new int[n];
+}
