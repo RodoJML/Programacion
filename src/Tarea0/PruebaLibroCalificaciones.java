ba1abd0772885011d31cc4ab254a177f1f36fd50
Rodolfo J. Meneses Leal <rodojml@me.com>
2021-04-09 17:54:52 -0600

    Homework for April 9th Pasar al NetBeans, ejecutar y hacer varias modificiones al ejemplo LibroCalificaciones (7.10 y 7.12)

---
+++ src/Tarea0/PruebaLibroCalificaciones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarea0;
+
+/**
+ *
+ * @author rodo
+ */
+public class PruebaLibroCalificaciones
+{
+    // el método main comienza la ejecución del programa
+    
+    public static void main(String[] args)
+    {
+        // arreglo de calificaciones de estudiantes
+        int[] arregloCalif = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones
+        ( "CS101 Introduccion a la programacion en Java", arregloCalif);
+        
+        System.out.printf("Bienvenido al libro de calificaciones para%n%s%n%n",
+          miLibroCalificaciones.obtenerNombreDelCurso());
+        
+        miLibroCalificaciones.procesarCalificaciones();
+    }
+    
+}
